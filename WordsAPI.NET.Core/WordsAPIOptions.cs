99872ece9b560308b6f975774b5e808d4b01f05b
EvgenSk <evgensk@gmail.com>
2019-08-07 15:36:34 +0700

    add summary to options

--- WordsAPI.NET.Core/WordsAPIOptions.cs
+++ WordsAPI.NET.Core/WordsAPIOptions.cs
@@ -6,8 +6,19 @@ namespace WordsAPI.NET.Core
 {
     public class WordsAPIOptions
     {
+        /// <summary>
+        /// Default is "https://wordsapiv1.p.mashape.com/"
+        /// </summary>
         public string BaseURL { get; set; } = "https://wordsapiv1.p.mashape.com/";
+
+        /// <summary>
+        /// Default is "wordsapiv1.p.mashape.com"
+        /// </summary>
         public string MashapeHost { get; set; } = "wordsapiv1.p.mashape.com";
+
+        /// <summary>
+        /// You should use your key here
+        /// </summary>
         public string MashapeKey { get; set; }
     }
 }
