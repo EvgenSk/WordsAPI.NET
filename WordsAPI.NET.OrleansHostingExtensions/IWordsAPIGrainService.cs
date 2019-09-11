ec8213fc8a4133dcdb92c4797369fb769fe2140b
EvgenSk <evgensk@gmail.com>
2019-09-11 23:11:31 +0700

    - update Orleans dependencies
    - add GrainService skeleton

---
+++ WordsAPI.NET.OrleansHostingExtensions/IWordsAPIGrainService.cs
@@ -0,0 +1,15 @@
+﻿using Orleans.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WordsAPI.NET.Core;
+
+namespace WordsAPI.NET.OrleansHostingExtensions
+{
+    public interface IWordsAPIGrainService : IGrainService
+    {
+        Task<string> GetWordInfoAsync(string word, Endpoint endpoint = Endpoint.Everything);
+        Task<T> GetWordInfoAsync<T>(string word, Endpoint endpoint = Endpoint.Everything);
+    }
+}
---
+++ WordsAPI.NET.OrleansHostingExtensions/WordsAPIGrainService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Concurrency;
+using Orleans.Core;
+using Orleans.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WordsAPI.NET.Core;
+
+namespace WordsAPI.NET.OrleansHostingExtensions
+{
+    [Reentrant]
+    public class WordsAPIGrainService : GrainService, IWordsAPIGrainService
+    {
+        readonly IGrainFactory GrainFactory;
+
+        public WordsAPIGrainService(IServiceProvider services, IGrainIdentity id, Silo silo, ILoggerFactory loggerFactory, IGrainFactory grainFactory) 
+            : base(id, silo, loggerFactory)
+        {
+            GrainFactory = grainFactory;
+        }
+
+        public Task<string> GetWordInfoAsync(string word, Endpoint endpoint = Endpoint.Everything)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetWordInfoAsync<T>(string word, Endpoint endpoint = Endpoint.Everything)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
