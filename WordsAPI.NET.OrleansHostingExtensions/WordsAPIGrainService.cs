7f9732b942e3823e6f70548e4d3f9fe5bb2c96f2
EvgenSk <evgensk@gmail.com>
2019-09-12 22:43:53 +0700

    implement draft for WordsAPIGrainService

--- WordsAPI.NET.OrleansHostingExtensions/WordsAPIGrainService.cs
+++ WordsAPI.NET.OrleansHostingExtensions/WordsAPIGrainService.cs
@@ -15,21 +15,19 @@ namespace WordsAPI.NET.OrleansHostingExtensions
     public class WordsAPIGrainService : GrainService, IWordsAPIGrainService
     {
         readonly IGrainFactory GrainFactory;
+        readonly WordsAPIClient Client;
 
         public WordsAPIGrainService(IServiceProvider services, IGrainIdentity id, Silo silo, ILoggerFactory loggerFactory, IGrainFactory grainFactory) 
             : base(id, silo, loggerFactory)
         {
             GrainFactory = grainFactory;
+            Client = WordsAPIClientFactory.Create(services);
         }
 
-        public Task<string> GetWordInfoAsync(string word, Endpoint endpoint = Endpoint.Everything)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<string> GetWordInfoAsync(string word, Endpoint endpoint = Endpoint.Everything) =>
+            Client.GetWordInfoAsync(word, endpoint);
 
-        public Task<T> GetWordInfoAsync<T>(string word, Endpoint endpoint = Endpoint.Everything)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<T> GetWordInfoAsync<T>(string word, Endpoint endpoint = Endpoint.Everything) =>
+            Client.GetWordInfoAsync<T>(word, endpoint);
     }
 }
